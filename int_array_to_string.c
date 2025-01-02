78d39f4c926b7536ffe437674e6a76d94aebb4d6
Kanta Ara <kaara@c3r4s8.42tokyo.jp>
2025-01-02 15:02:52 +0900

    add arr_spc_len

--- int_array_to_string.c
+++ int_array_to_string.c
@@ -6,11 +6,13 @@
 /*   By: kaara <kaara@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/02 14:25:58 by kaara             #+#    #+#             */
-/*   Updated: 2025/01/02 14:28:06 by kaara            ###   ########.fr       */
+/*   Updated: 2025/01/02 15:01:58 by kaara            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static ssize_t	arr_spc_len(int *arr);
 
 char	*int_array_to_string(int *arr)
 {
@@ -37,4 +39,19 @@ char	*int_array_to_string(int *arr)
 			line[line_i + 1] = '\0';
 	}
 	return (line);
+}
+
+static ssize_t	arr_spc_len(int *arr)
+{
+	ssize_t	arr_i;
+	ssize_t	string_len;
+
+	arr_i = 1;
+	while (arr_i <= arr[0])
+	{
+		string_len += intlen(arr[arr_i]);
+		arr_i++;
+	}
+	string_len += arr[0] - 1;
+	return (string_len);
 }
