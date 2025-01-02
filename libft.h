9bf9b2f885b6594c4018a1c06381cb92648f10da
Kanta Ara <kaara@c3r4s8.42tokyo.jp>
2025-01-02 16:05:37 +0900

    update .h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: kaara <kaara@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/25 18:25:11 by kaara             #+#    #+#             */
-/*   Updated: 2025/01/02 14:32:37 by kaara            ###   ########.fr       */
+/*   Updated: 2025/01/02 16:05:12 by kaara            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,5 +57,6 @@ size_t	free_and_reset(char **buffer);
 char	*get_next_line(int fd);
 ssize_t	intlen(int src);
 char	*int_array_to_string(int *arr);
+char	*zero_pad_itoa(int n, ssize_t size);
 
 #endif
