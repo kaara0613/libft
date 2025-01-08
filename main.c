7ebb05be80e1746ad5279ad710c5551ad9e20365
Kanta Ara <kaara@c3r3s7.42tokyo.jp>
2025-01-08 15:34:40 +0900

    1/8/15:34

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: kaara <kaara@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/05 10:30:52 by kaara             #+#    #+#             */
-/*   Updated: 2025/01/06 18:32:21 by kaara            ###   ########.fr       */
+/*   Updated: 2025/01/08 15:26:33 by kaara            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,9 @@ int	main(void)
 {
     int n[5] = {4, 13, 244, 443, 44444};
     char *str = int_array_to_string(n);
- 
-    printf("%s\n", str);
+
+    for (int i = 0; str[i] != '\n'; i++)
+        printf("%c\n", str[i]);
     free(str);
     return (0);
 }
